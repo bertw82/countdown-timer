ff769895ef608153fd409b8f9be377176fe9e276
Bert Witzel <bwitzel82@gmail.com>
2021-02-10 17:54:01 -0500

    fix countdown timer

--- js/app.js
+++ js/app.js
@@ -19,17 +19,19 @@ for (let i = 0; i < 4; i++){
 const date = new Date().getDate() + 9;
 const newDate = new Date().setDate(date);
 const futureDate = new Date(newDate); 
-const countdown = setInterval( () => {
-    const today = new Date().getTime();
-    const distance = futureDate - today;
-    const days = pad(Math.floor(distance / (1000 * 60 * 60 * 24)));
-    const hours = pad(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-    const minutes = pad(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
-    const seconds = pad(Math.floor((distance % (1000 * 60)) / 1000)); 
-    const timeArr = [days, hours, minutes, seconds];
-    const clocky = document.getElementsByClassName('clock-face');
-    for (let i = 0; i < clocky.length; i++){   
-        clocky[i].textContent = timeArr[i];
-    }
-}, 1000);
+function setClock() {
+  const today = new Date().getTime();
+  const distance = futureDate - today;
+  const days = pad(Math.floor(distance / (1000 * 60 * 60 * 24)));
+  const hours = pad(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+  const minutes = pad(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
+  const seconds = pad(Math.floor((distance % (1000 * 60)) / 1000)); 
+  const timeArr = [days, hours, minutes, seconds];
+  const clocky = document.getElementsByClassName('clock-face');
+  for (let i = 0; i < clocky.length; i++){   
+      clocky[i].textContent = timeArr[i];
+  }
+}
+setClock();
+setInterval(setClock, 1000);
 
