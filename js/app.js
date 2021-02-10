29194909eb84fba7c846545893a2e91a40e0f49e
Bert Witzel <bwitzel82@gmail.com>
2021-02-10 10:05:56 -0500

    rework clock function

--- js/app.js
+++ js/app.js
@@ -16,10 +16,12 @@ for (let i = 0; i < 4; i++){
 }
 
 // outline for this code from https://www.w3schools.com/howto/howto_js_countdown.asp
-const taxDay = new Date("Apr 15, 2021 12:00:00").getTime();
+const date = new Date().getDate() + 9;
+const newDate = new Date().setDate(date);
+const futureDate = new Date(newDate); 
 const countdown = setInterval( () => {
     const today = new Date().getTime();
-    const distance = taxDay - today;
+    const distance = futureDate - today;
     const days = pad(Math.floor(distance / (1000 * 60 * 60 * 24)));
     const hours = pad(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
     const minutes = pad(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
